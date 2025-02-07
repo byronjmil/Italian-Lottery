a5777ae9fae4a6642deacaabdd4f4a40e8705fd1
Byron Hall <byronhall@Byrons-Mac-mini.local>
2025-02-07 15:44:55 -0600

    added simulation to get every number

---
+++ FillUpSet.js
@@ -0,0 +1,11 @@
+const mySet = new Set();
+let drawings = 0;
+
+while(mySet.size !== 1000){
+    drawings += 1;
+    const uintarr = new Uint16Array(1);
+    crypto.getRandomValues(uintarr);
+    mySet.add(uintarr[0] % 1000);
+}
+
+console.log(`Number of drawings to complete: ${drawings}`); // 6506
