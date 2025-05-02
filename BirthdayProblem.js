81a5554c66ae3812eadbafc51f42c952c4b3ccd9
Byron Hall <byronhall@Byrons-Mac-mini.local>
2025-05-02 15:38:11 -0500

    birthday problem

---
+++ BirthdayProblem.js
@@ -0,0 +1,20 @@
+function birthdayProblem(n) {
+    const daysInYear = 1000; // Assume there are 365 days in a year
+    let probability = 1;
+
+    for (let i = 0; i < n; i++) {
+        // For each person, calculate the chance they don't share a birthday with someone else already in the group
+        probability *= (daysInYear - i) / daysInYear;
+    }
+
+    // The probability that at least two people share a birthday is the complement of the above probability
+    return 1 - probability;
+}
+let i = 1;
+let result = birthdayProblem(i);
+while (result < .5) {
+    i += 1;
+    result = birthdayProblem(i);
+}
+// Example usage:
+console.log(`${i} = ${result}`); // This will print the probability for 23 people
---
+++ TestingBirthdayProblem.js
@@ -0,0 +1,23 @@
+const previousPicks = require('./PastDrawings').globalArr;
+
+let winnerCount = 0;
+let loserCount = 0;
+
+let arr = [];
+let mySet = new Set();
+
+for (let i = 0; i < previousPicks.length; i += 1) {
+    if (i > 0 && i % 38 === 0) {
+        if (mySet.size === 38) {
+            loserCount += 1;
+        }
+        else {
+            winnerCount += 1;
+        }
+        mySet.clear();
+    }
+    mySet.add(previousPicks[i]);
+}
+
+console.log(winnerCount);
+console.log(loserCount);
