b1a77910bdd270fb460a046e1314af904afd08b9
Byron Hall <byronhall@Byrons-Mac-mini.local>
2025-05-02 18:03:42 -0500

    added snapshots

---
+++ Snapshots.js
@@ -0,0 +1,33 @@
+function getSnapshots(arr, snapshotSize) {
+    const snapshots = [];
+
+    // Loop through the array and create snapshots
+    for (let i = 0; i <= arr.length - snapshotSize; i++) {
+        snapshots.push(arr.slice(i, i + snapshotSize)); // Take a snapshot of size `snapshotSize`
+    }
+
+    return snapshots;
+}
+
+const arr = require('./PastDrawings').globalArr;
+const snapshotSize = 100;
+const snapshots = getSnapshots(arr, snapshotSize);
+
+let winnerCount = 0;
+let loserCount = 0;
+
+snapshots.forEach(snapshot => {
+    const mySet = new Set();
+    snapshot.forEach(element => {
+        mySet.add(element);
+    });
+    if (mySet.size < 100) {
+        winnerCount += 1;
+    }
+    else {
+        loserCount += 1;
+    }
+});
+
+console.log(winnerCount);
+console.log(loserCount);
