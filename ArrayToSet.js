798a9308256a8bf177fd278f2db5f364a73263e5
Byron Hall <byronhall@Byrons-Mac-mini.local>
2025-05-02 13:18:53 -0500

    may 2

--- ArrayToSet.js
+++ ArrayToSet.js
@@ -28,4 +28,4 @@ while (myPicks.size < 5) {
 console.log(myPicks);
 
 // console.log(previousPicks.length);
-// console.log(previousPicks.includes('627'))
+// console.log(previousPicks.includes('820'))
